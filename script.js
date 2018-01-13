032ea7cf770604dff12cce16be09b01ee577cd66
viktoria Herzer <viktoria.herzer@gmail.com>
2018-01-13 15:53:58 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,178 @@
+/*CDs Storage*/
+{
+
+    "CDs": [{
+
+        "Title": "X & Y",
+
+        "Artist": "Coldplay",
+
+        "Genre": "Alternative Rock",
+
+        "Label": "Parlophone",
+
+        "Minutes": "63",
+
+        "Rating": 0
+
+    }, {
+
+        "Title": "Thriller",
+
+        "Artist": "Michael Jackson",
+
+        "Genre": "Pop",
+
+        "Label": "Epic",
+
+        "Minutes": "42",
+
+        "Rating": 0
+
+      }, {
+
+        "Title": "In the Lonely Hour",
+
+        "Artist": "Sam Smith",
+
+        "Genre": "Pop",
+
+        "Label": "Capitol",
+
+        "Minutes": "32",
+
+        "Rating": 0
+
+    }   
+
+    }]
+
+}
+/*Books Storage*/
+{
+
+    "Books": [{
+
+        "Title": "The Great Gatsby",
+
+        "Author": "F.Scott Fitzgerald",
+
+        "Genre": "Novel",
+
+        "Publisher": "Charles Scribner's Sons",
+
+        "Rating": 0
+
+
+    }, {
+
+        "Title": "The Reader",
+
+        "Author": "Bernhard Schlink",
+
+        "Genre": "Novel",
+
+        "Publisher": "Vintage International",
+
+        "Rating": 0
+
+      }, {
+
+        "Title": "The Lion, The Witch and the Wardrobe",
+
+        "Author": "C.S. Lewis",
+
+        "Genre": "Childrens Fantasy",
+
+        "Publisher": "Geoffrey Bles",
+
+        "Rating": 0
+
+    }   
+
+    }]
+
+}
+/*DVDs Storage*/
+{
+
+    "DVDs": [{
+
+        "Title": "The Wolf of Wall Street",
+
+        "Director": "Martin Scorsese",
+
+        "Genre": "Comedy",
+
+        "Rating": 0
+
+    }, {
+
+        "Title": "Inception",
+
+        "Director": "Christopher Nolan",
+
+        "Genre": "Sci-Fi / Thriller",
+
+        "Rating": 0
+
+      }, {
+
+        "Title": "Titanic",
+
+        "Director": "James Cameron",
+
+        "Genre": "Disaster Film/ Romance",
+
+        "Rating": 0
+
+    }   
+
+    }]
+
+}
+/* Comics Storage*/
+{
+
+    "Comics": [{
+
+        "Title": "Batman",
+
+        "Genre": "Superhero",
+
+        "Publisher": "DC Comics",
+
+        "Author": "Bill Finger",
+
+        "Rating": 0
+
+    }, {
+
+        "Title": "Ant-Man",
+
+        "Genre": "Superhero",
+
+        "Publisher": "Marvel Comics",
+
+        "Author": "Stan Lee",
+
+        "Rating": 0
+
+      }, {
+
+        "Title": "Wonder Woman",
+
+        "Genre": "Superhero",
+
+        "Publisher": "DC Comics",
+
+        "Author": "William Moulton Marston",
+
+        "Rating": 0
+
+    }   
+
+    }]
+
+}
+
